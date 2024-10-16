3263284c226d29b07d13efc0d96d62ea9535a949
Tresor Muco <mucotresor@gmail.com>
2024-10-16 10:39:43 +0200

    chore: setup boot configs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +28,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        // Configure commands
+        DB::prohibitDestructiveCommands(app()->isProduction());
+
+        // Configure models
+        Model::shouldBeStrict(! app()->isProduction());
+        Model::unguard();
+
+        // Configure Password Validation rule
+        Password::defaults(function () {
+            return app()->isProduction() ? Password::min(8)->uncompromised() : null;
+        });
+        // Configure dates
+        Date::use(CarbonImmutable::class);
     }
 }
