6c02b37edfd78fe7e27bbbe8c71915adc34e7a7b
Tresor Muco <mucotresor@gmail.com>
2024-10-18 09:57:04 +0200

    refactor: refactor PasswordConfirmationTest to Pest functional Syntax

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -2,31 +2,29 @@
 
 use App\Models\User;
 
-test('confirm password screen can be rendered', function () {
+use function Pest\Laravel\actingAs;
+
+it('can render the confirm password screen', function () {
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->get('/confirm-password');
-
-    $response->assertStatus(200);
+    actingAs($user)
+        ->get(route("password.confirm"))
+        ->assertOk();
 });
 
-test('password can be confirmed', function () {
+it('can confirm the password', function () {
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->post('/confirm-password', [
-        'password' => 'password',
-    ]);
-
-    $response->assertRedirect();
-    $response->assertSessionHasNoErrors();
+    actingAs($user)
+        ->post(route("password.confirm"), ['password' => 'password'])
+        ->assertRedirect()
+        ->assertSessionHasNoErrors();
 });
 
-test('password is not confirmed with invalid password', function () {
+it('cannot confirm an invalid password', function () {
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->post('/confirm-password', [
-        'password' => 'wrong-password',
-    ]);
-
-    $response->assertSessionHasErrors();
+    actingAs($user)
+        ->post(route("password.confirm"), ['password' => 'wrong-password'])
+        ->assertSessionHasErrors();
 });
