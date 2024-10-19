2478a5e259024b33ca29c6e544f67652d8f15c69
Tresor Muco <mucotresor@gmail.com>
2024-10-20 01:21:14 +0200

    fix(ProfileTest): fixed failing test in ProfileTest

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertGuest;
@@ -9,69 +11,71 @@ use function PHPUnit\Framework\assertNull;
 use function PHPUnit\Framework\assertSame;
 
 it('can display the profile page', function () {
-  $user = User::factory()->create();
+    $user = User::factory()->create();
 
-  actingAs($user)
-    ->get(route("profile.edit"))
-    ->assertOk();
+    actingAs(type($user)->as(Authenticatable::class))
+        ->get(route("user.profile"))
+        ->assertOk();
 });
 
 it('can update the profile information', function () {
-  $user = User::factory()->create();
+    $user = User::factory()->create();
 
-  $response = actingAs($user)
-    ->patch(route("profile.update"), [
-      'name' => 'Test User',
-      'email' => 'test@example.com',
-    ])
-    ->assertSessionHasNoErrors()
-    ->assertRedirect(route("profile.edit"));
+    actingAs(type($user)->as(Authenticatable::class))
+        ->from(route("user.profile"))
+        ->patch(route("profile.update"), [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ])
+        ->assertSessionHasNoErrors()
+        ->assertRedirect(route("user.profile"));
 
-  $user->refresh();
+    $user->refresh();
 
-  assertSame('Test User', $user->name);
-  assertSame('test@example.com', $user->email);
-  assertNull($user->email_verified_at);
+    assertSame('Test User', $user->name);
+    assertSame('test@example.com', $user->email);
+    assertNull($user->email_verified_at);
 });
 
 it('does not change the email verification status if the email address remains the same', function () {
-  $user = User::factory()->create();
+    $user = User::factory()->create();
 
-  actingAs($user)
-    ->patch(route("profile.edit"), [
-      'name' => 'Test User',
-      'email' => $user->email,
-    ])
-    ->assertSessionHasNoErrors()
-    ->assertRedirect(route("profile.edit"));
+    actingAs(type($user)->as(Authenticatable::class))
+        ->from(route("user.profile"))
+        ->patch(route("profile.update"), [
+            'name' => 'Test User',
+            'email' => $user->email,
+        ])
+        ->assertSessionHasNoErrors()
+        ->assertRedirect(route("user.profile"));
 
-  assertNotNull($user->refresh()->email_verified_at);
+    assertNotNull($user->refresh()->email_verified_at);
 });
 
 it('can delete a user-owned account', function () {
-  $user = User::factory()->create();
+    $user = User::factory()->create();
 
-  actingAs($user)
-    ->delete(route("profile.destroy"), [
-      'password' => 'password',
-    ])
-    ->assertSessionHasNoErrors()
-    ->assertRedirect(route("welcome"));
+    actingAs(type($user)->as(Authenticatable::class))
+        ->delete(route("profile.destroy"), [
+            'password' => 'password',
+        ])
+        ->assertSessionHasNoErrors()
+        ->assertRedirect(route("welcome"));
 
-  assertGuest();
-  assertNull($user->fresh());
+    assertGuest();
+    assertNull($user->fresh());
 });
 
 it('must provide the correct password to delete account', function () {
-  $user = User::factory()->create();
+    $user = User::factory()->create();
 
-  actingAs($user)
-    ->from(route("profile.edit"))
-    ->delete(route("profile.destroy"), [
-      'password' => 'wrong-password',
-    ])
-    ->assertSessionHasErrors('password')
-    ->assertRedirect('/profile');
+    actingAs(type($user)->as(Authenticatable::class))
+        ->from(route("user.settings"))
+        ->delete(route("profile.destroy"), [
+            'password' => 'wrong-password',
+        ])
+        ->assertSessionHasErrors('password')
+        ->assertRedirect(route("user.settings"));
 
-  assertNotNull($user->fresh());
+    assertNotNull($user->fresh());
 });
