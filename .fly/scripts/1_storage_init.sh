d5dc74b2af498d548764ab21146f49ac09233650
Tresor Muco <mucotresor@gmail.com>
2024-10-20 19:10:42 +0200

    chore: remove unwanted setup in fly.toml

--- .fly/scripts/1_storage_init.sh
+++ .fly/scripts/1_storage_init.sh
@@ -1,56 +1,14 @@
-#!/bin/bash
-
-# Ensure the storage folder has the correct structure
-STORAGE_PATH=/var/www/html/storage
-STORAGE_BACKUP_PATH=/var/www/html/storage_
-
-if [ ! -d "$STORAGE_PATH/app" ]; then
-    echo "$STORAGE_PATH/app is not a directory, copying content from $STORAGE_BACKUP_PATH to $STORAGE_PATH"
-    cp -r $STORAGE_BACKUP_PATH/. $STORAGE_PATH
-    echo "Deleting $STORAGE_BACKUP_PATH..."
-    rm -rf $STORAGE_BACKUP_PATH
+FOLDER=/var/www/html/storage/app
+if [ ! -d "$FOLDER" ]; then
+    echo "$FOLDER is not a directory, copying storage_ content to storage"
+    cp -r /var/www/html/storage_/. /var/www/html/storage
+    echo "deleting storage_..."
+    rm -rf /var/www/html/storage_
 fi
 
-# Ensure the database folder and SQLite file exist
-DB_FOLDER=$STORAGE_PATH/database
-DB_FILE=$DB_FOLDER/database.sqlite
-
-if [ ! -d "$DB_FOLDER" ]; then
-    echo "$DB_FOLDER does not exist, initializing database folder"
-    mkdir -p $DB_FOLDER
+FOLDER=/var/www/html/storage/database
+if [ ! -d "$FOLDER" ]; then
+    echo "$FOLDER is not a directory, initializing database"
+    mkdir /var/www/html/storage/database
+    touch /var/www/html/storage/database/database.sqlite
 fi
-
-if [ ! -f "$DB_FILE" ]; then
-    echo "$DB_FILE not found, creating SQLite database"
-    touch $DB_FILE
-fi
-
-echo "Script execution completed."
-#!/bin/bash
-
-# Ensure the storage folder has the correct structure
-STORAGE_PATH=/var/www/html/storage
-STORAGE_BACKUP_PATH=/var/www/html/storage_
-
-if [ ! -d "$STORAGE_PATH/app" ]; then
-    echo "$STORAGE_PATH/app is not a directory, copying content from $STORAGE_BACKUP_PATH to $STORAGE_PATH"
-    cp -r $STORAGE_BACKUP_PATH/. $STORAGE_PATH
-    echo "Deleting $STORAGE_BACKUP_PATH..."
-    rm -rf $STORAGE_BACKUP_PATH
-fi
-
-# Ensure the database folder and SQLite file exist
-DB_FOLDER=$STORAGE_PATH/database
-DB_FILE=$DB_FOLDER/database.sqlite
-
-if [ ! -d "$DB_FOLDER" ]; then
-    echo "$DB_FOLDER does not exist, initializing database folder"
-    mkdir -p $DB_FOLDER
-fi
-
-if [ ! -f "$DB_FILE" ]; then
-    echo "$DB_FILE not found, creating SQLite database"
-    touch $DB_FILE
-fi
-
-echo "Script execution completed."
